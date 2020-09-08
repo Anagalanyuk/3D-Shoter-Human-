0ebafb330e92bc95b5432303aaa42c4b61634758
AnatoliyGala <anagalanyuk@gmail.com>
2020-09-08 13:45:21 +0300

    add rotation

---
+++ New Unity Project3Dshoter(Human)/Assets/Scripts/OrbitalCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    public float _rotSpeed = 1.5f;
+    private float _rotY;
+    private Vector3 _offSet;
+
+    private void Start()
+    {
+        _rotY = _target.eulerAngles.y;
+        _offSet = _target.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+
+        if(horInput != 0)
+        {
+            _rotY += horInput * _rotSpeed;
+        }
+        else
+        {
+            _rotY += Input.GetAxis("Mouse X") * _rotSpeed * 3;
+        }
+
+        Quaternion rotation = Quaternion.Euler(0, _rotY, 0);
+        transform.position = _target.position - (rotation * _offSet);
+        transform.LookAt(_target);
+    }
+}
