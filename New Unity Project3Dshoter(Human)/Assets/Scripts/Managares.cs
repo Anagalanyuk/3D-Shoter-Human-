718bd94d91b8551e7a0d8e27db42036ab0fc258e
AnatoliyGala <anagalanyuk@gmail.com>
2020-09-09 11:33:38 +0300

    Add pattern MVC

---
+++ New Unity Project3Dshoter(Human)/Assets/Scripts/Managares.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Playermanager))]
+[RequireComponent(typeof(InventoryManager))]
+
+public class Managares : MonoBehaviour
+{
+    public static Playermanager Player { get; private set; }
+    public static InventoryManager Inventory { get; private set; }
+
+    private List<IGameManager> _startSequence;
+
+    private void Awake()
+    {
+        Player = GetComponent<Playermanager>();
+        Inventory = GetComponent<InventoryManager>();
+
+        _startSequence = new List<IGameManager>();
+        _startSequence.Add(Player);
+        _startSequence.Add(Inventory);
+
+        StartCoroutine(StartupManagers());
+
+
+    }
+   
+    private IEnumerator StartupManagers()
+    {
+        foreach(IGameManager manager in _startSequence)
+        {
+            manager.Startup();
+        }
+
+        yield return null;
+
+        int numModuls = _startSequence.Count;
+        int numReady = 0;
+
+        while(numReady < numModuls)
+        {
+            int lastRedy = numReady;
+            numReady = 0;
+        }
+
+
+    }
+}
---
+++ New Unity Project3Dshoter(Human)/Assets/Scripts/Playermanager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Playermanager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+
+    public int _helth { get; private set; }
+    public int _maxHelth { get; private set; }
+
+    public void Startup ()
+    {
+        Debug.Log("Player mnager starting...");
+
+        _helth = 50;
+        _maxHelth = 100;
+
+        status = ManagerStatus.Started;
+    }
+
+    public void ChangeHalth(int value)
+    {
+        if(_helth > _maxHelth)
+        {
+            _helth = _maxHelth;
+        }
+        else if (_helth < 0)
+        {
+            _helth = 0;
+        }
+
+        Debug.Log("Helth:" + _helth + "/" + _maxHelth);
+    }
+}
